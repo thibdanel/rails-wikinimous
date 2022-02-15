55b5f7bb2d45cdd729d44cd6fab397e8684d358e
Thibault Danel <thibdanel@hotmail.fr>
2022-02-15 21:41:45 +0100

    my first rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_task
+  get 'articles/:id', to: 'articles#show', as: :article
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
